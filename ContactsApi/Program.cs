6d1bd6daa3214883ee815b80d4ace4861493c4f5
olumuyiwa-agboola <agboolaod@gmail.com>
2024-12-13 09:23:14 +0100

    Add Scalar OpenAPI documentation support

--- ContactsApi/Program.cs
+++ ContactsApi/Program.cs
@@ -1,12 +1,13 @@
 using FastEndpoints;
+using Scalar.AspNetCore;
 using ContactsApi.Core.Services;
 using ContactsApi.Infrastructure;
 using ContactsApi.Core.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddFastEndpoints();
+builder.Services.AddOpenApi("scalar-open-api-doc");
 builder.Services.AddScoped<IContactsService, ContactsService>();
 builder.Services.AddScoped<IContactsService, ContactsService>();
 builder.Services.AddScoped<IContactsRepository, ContactsRepository>();
@@ -15,7 +16,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); 
+    app.MapScalarApiReference(options =>
+    {
+        options.WithEndpointPrefix("/{documentName}");
+    });
 }
 
 app.UseHttpsRedirection();
